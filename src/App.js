bc7ec615007cf146c05a316266b9b381256f4db1
neeleshksingh <neelesh1517@gmail.com>
2023-02-27 19:25:23 +0530

    seventh commit

--- src/App.js
+++ src/App.js
@@ -7,42 +7,61 @@ import Weather from './components/weather';
 function App() {
   const [search, setSearch] = useState('')
   const [weather, setWeather] = useState([])
+  const [lastSearch, setLastSearch] = useState([])
+  const [showlast, setShowlast] = useState(false)
 
-  const handleSearch = () =>{
-    if(search !== ''){
+  const handleSearch = () => {
+    if (search !== '') {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=8c078b7d5c71f260570371ee6b6b765b`)
-      .then(res => res.json())
-      .then(data =>{
-        if(data.cod === 200){
-          setWeather(data)
-
+        .then(res => res.json())
+        .then(data => {
+          if (data.cod === 200) {
+            setWeather(data)
+            setLastSearch(prev => [search, ...prev.slice(0, 2)])
+            setShowlast(true)
+          }
+          else {
+            alert("Enter Valid City Name")
+          }
+        }).catch(e => {
+          alert(e)
+        })
+    }
+    else {
+      setShowlast(false)
+      setLastSearch(prev => {
+        const update = [...prev]
+        if (update.length === 3) {
+          update.pop()
         }
-        else{
-          alert("Enter Valid City Name")
-        }
-      }).catch(e=>{
-        alert(e)
+        update.unshift('')
+        return update
       })
     }
   }
-
-
   return (
     <div className="App">
       <div className='con1 flex-col'>
         <h1>Weather App</h1>
         <div className='search'>
-          <input type="text" placeholder='Enter City Name' value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
+          <input type="text" placeholder='Enter City Name' value={search} onChange={(e) => { setSearch(e.target.value) }} />
           <button onClick={handleSearch}>Search</button>
         </div>
       </div>
       <div className='con2 flex-col'>
-        <Weather weatherdata = {weather}/>
+        <Weather weatherdata={weather} />
       </div>
-      <div className='last-3'>
-          <h2 style={{color: 'blue'}}>Last 3 City entries</h2>
+      {showlast && (
+        <div className='last-3 flex-col'>
+          <h2 style={{ color: 'blue' }}>Last 3 City entries</h2>
+          <ul>
+            {lastSearch.map((city, index) => {
+              return <li key={index}>{city}</li>
+            })}
+          </ul>
+        </div>
+      )}
 
-      </div>
     </div>
   );
 }
